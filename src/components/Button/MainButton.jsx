e9233a58bd64c457878c25efb2cd6d8ef0088914
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-08-23 15:38:17 +0530

    update-[vidushi]
    button

--- src/components/Button/MainButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MainButton = () => {
-  return (
-    <div>MainButton</div>
-  )
-}
-
-export default MainButton
--- src/components/Button/PrimaryButton.jsx
+++ src/components/Button/PrimaryButton.jsx
@@ -4,7 +4,7 @@ const PrimaryButton = ({link, btnText, className}) => {
     return(
         <Link href={link} className={`btn ${className}`}>
             <div aria-hidden="true" className="btn-circle">
-                <div className="btn-circle-text">
+                <div className="btn-circle-text ">
                     <svg viewBox="0 0 22 16" fill="current-color" xmlns="http://www.w3.org/2000/svg" className="btn-icon">
                         <g className="btn-path" fill="current-color">
                             <path d="M13.6437 15.9303C13.4217 15.9303 13.1998 15.8485 13.0246 15.6733C12.6858 15.3345 12.6858 14.7738 13.0246 14.4351L19.496 7.96366L13.0246 1.49228C12.6858 1.15352 12.6858 0.592823 13.0246 0.254067C13.3633 -0.0846889 13.924 -0.0846889 14.2628 0.254067L21.3533 7.34455C21.6921 7.68331 21.6921 8.24401 21.3533 8.58277L14.2628 15.6733C14.0876 15.8485 13.8656 15.9303 13.6437 15.9303Z" fill="current-color"/>
