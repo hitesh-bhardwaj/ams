1e06234bdfcbbecede725ccb01b6d16dd91e271d
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-10-21 19:33:11 +0530

    update
    enabled pixi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -52,7 +52,7 @@ export default function App({ Component, pageProps, router }) {
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </ReactLenis>
-     {/* <Pixifinal /> */}
+     <Pixifinal />
      
     </>
   );
