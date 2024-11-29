a7a0bd97fa26f7cd466629c47b1d488fb01a58da
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-11-29 19:33:22 +0530

    update-[vidushi]

--- src/components/CardioVascular/ProductCarousel.jsx
+++ src/components/CardioVascular/ProductCarousel.jsx
@@ -117,9 +117,9 @@ const ProductCarousel = () => {
 
 
   return (
-    <section id="productCarousel" className="relative w-screen h-screen overflow-hidden mobile:h-[130vh] mobile:py-[10vw] bg-white/30 py-[5vw] mt-[7%] mobile:mb-[15%] tablet:h-[70vh] ">
+    <section id="productCarousel" className="relative w-screen h-[110vh] overflow-hidden  mobile:h-[150vh] mobile:py-[20vw] bg-white/30 py-[10vw] mt-[7%]  mobile:mb-[15%] tablet:h-[70vh] ">
       <div className="w-full h-full flex items-center justify-between pl-[5vw] mobile:flex-col-reverse tablet:flex-col-reverse">
-        <div className="w-[50%] mobile:w-full mobile:h-[50vh] tablet:w-full tablet:h-[50vh]">
+        <div className="w-[50%] mobile:w-full mobile:h-[60vh] tablet:w-full tablet:h-[50vh]">
           <div className="mobile:flex mobile:flex-col mobile:items-center mobile:justify-center">
             <h2 className="title-2 aeonik mobile:text-center mobile:ml-0 mobile:px-[5vw] ">
               <span data-para-anim>{slidesData[activeSlide]?.title}</span>
@@ -147,7 +147,7 @@ const ProductCarousel = () => {
             direction={"vertical"}
             spaceBetween={20}
             centeredSlides={true}
-            className={`cardiovascularSwiper w-full h-full absolute left-[30%] top-[3%] rounded-[30px] mobile:left-0 mobile:h-[100%] tablet:left-0 tablet:h-full`}
+            className={`cardiovascularSwiper w-full h-full absolute left-[30%] top-[3%] rounded-[30px] mobile:left-0 mobile:h-[100%] tablet:left-0 tablet:h-full mobile:pointer-events-none`}
             onSlideChange={handleSlideChange}
             // onSwiper={handleSlideChange}
             onSwiper={(swiper) => (swiperRef.current = swiper)}
@@ -155,6 +155,7 @@ const ProductCarousel = () => {
               541: {
                 slidesPerView: 1,
                 spaceBetween:30,
+                
               },
               640:{
                   slidesPerView:1,
@@ -175,7 +176,7 @@ const ProductCarousel = () => {
             ))}
           </Swiper>
           <div
-            className={`px-[1.6vw] py-[1.6vw] absolute z-[5] bottom-[20%] right-[30%] mobile:top-[50%] mobile:right-auto mobile:left-[62%] translate-x-[-65%] mobile:bottom-auto tablet:top-[10%] overflow-hidden mobile:p-[5vw] tablet:p-[2vw] rounded-full next-button cursor-pointer  mobile:block group hover:text-white bg-white/50 tablet:right-0 tablet:bottom-auto
+            className={`px-[1.6vw] py-[1.6vw] absolute z-[5] bottom-[30%] right-[30%] mobile:top-[45%] mobile:right-auto mobile:left-[62%] translate-x-[-65%] mobile:bottom-auto tablet:top-[10%] overflow-hidden mobile:p-[5vw] tablet:p-[2vw] rounded-full next-button cursor-pointer  mobile:block group hover:text-white bg-white/50 tablet:right-0 tablet:bottom-auto
                 ${
                   activeButton === "next"
                     ? " text-white"
@@ -202,7 +203,7 @@ const ProductCarousel = () => {
             </div>
           </div>
           <div
-            className={`px-[1.6vw] py-[1.6vw] top-[20%] right-[32%] absolute z-[5] mobile:bottom-auto mobile:right-auto mobile:top-[50%] mobile:left-[39%] translate-x-[-38%]  tablet:top-[10%] bg-white/50 overflow-hidden  mobile:p-[5vw] tablet:p-[2vw] rounded-full prev-button cursor-pointer  mobile:block group hover:text-white tablet:right-[10%]
+            className={`px-[1.6vw] py-[1.6vw] top-[25%] right-[32%] absolute z-[5] mobile:bottom-auto mobile:right-auto mobile:top-[45%] mobile:left-[39%] translate-x-[-38%]  tablet:top-[10%] bg-white/50 overflow-hidden  mobile:p-[5vw] tablet:p-[2vw] rounded-full prev-button cursor-pointer  mobile:block group hover:text-white tablet:right-[10%]
                 ${
                   activeButton === "prev"
                     ? " text-white"
@@ -230,7 +231,7 @@ const ProductCarousel = () => {
           </div>
         </div>
       </div>
-      <div className="absolute w-[45vw] h-full top-0 right-0 z-[-1] mobile:h-[50vh] mobile:w-[90vw] mobile:top-[10%] tablet:hidden">
+      <div className="absolute w-[46vw] h-[90vh] top-[10%] right-0 z-[-1] mobile:h-[50vh] mobile:w-[90vw] mobile:top-[10%] tablet:hidden">
         <Image
           src="/assets/cardioVascular/product-bg.png"
           fill
